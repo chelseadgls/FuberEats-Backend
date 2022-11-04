a9c1fa798af8217eea273ef645a4d47850b7f413
Tony Wu <tonywu@Tonys-MacBook-Air-2.local>
2022-11-04 14:41:17 -0400

    addded schema for user info and updated routes, and index, and models, and created controller functions

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,12 @@
 import { Router } from "express";
 import productRoutes from "./products.js";
+import userRoutes from "./userInfo.js"
 
 const router = Router();
 
 router.get("/", (req, res) => res.send("This is the api root!"));
 
 router.use("/products", productRoutes);
+router.use("/user". userRoutes)
 
 export default router;
---
+++ routes/userInfo.js
@@ -0,0 +1,16 @@
+import { Router } from "express";
+import * as controllers from "../controllers/userInfo.js"
+const router = Router();
+
+router.get("/", controllers.getUsers);
+
+router.post("/", controllers.createUser);
+
+router.get("/:id", controllers.getUserByHash);
+
+router.put("/:id", controllers.updateUser);
+
+router.delete("/:id", controllers.deleteUser);
+
+
+export default router;
