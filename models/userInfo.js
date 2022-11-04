91de21e8902f681c0bb01a6471df1d4fb7bed28e
Tony Wu <tonywu@Tonys-MacBook-Air-2.local>
2022-11-04 15:45:09 -0400

    updated schema for users

--- models/userInfo.js
+++ models/userInfo.js
@@ -3,7 +3,8 @@ import mongoose, { mongo } from "mongoose";
 const Schema = mongoose.Schema;
 
 let userInfo = new Schema({
-  hash: { type: String }
+  hash: { type: String },
+  cartItemsByID: [{ type: String }]
 });
 
 export default mongoose.model("userInfo", userInfo);
